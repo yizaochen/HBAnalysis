d7d7579d634d127ab3d46cb276aad619918ff00f
yizaochen <yizaochen@gmail.com>
2020-10-26 19:53:45 +0800

    Initialize

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # HBAnalysis
+
+# Create a virtual environment
+`conda create --name hbanalysis python=3.8`
+
+# Activate virtual environment
+`conda activate hbanalysis`
+
+# Deactivate
+`conda deactivate`
+
+# Install bentdna package
+`pip install -e .`
+
+# Upgrade package
+`pip install -e . --upgrade`
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='hbanalysis', 
+      version='0.1',
+      packages=find_packages(),
+      url='https://github.com/yizaochen/HBAnalysis.git',
+      author='Yizao Chen',
+      author_email='yizaochen@gmail.com',
+      install_requires=[
+          'MDAnalysis',
+          'matplotlib',
+          'pandas',
+          'scipy'
+      ]
+      )
